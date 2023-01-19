7eb07672bbdcdbb471bcc6d5ae693e9fc0afce65
Dalton <daltonstwilliams@gmail.com>
2023-01-18 20:05:24 -0600

    2022 day 5

---
+++ 2022/5/2.py
@@ -0,0 +1,45 @@
+import time
+
+
+def main():
+    f = open('input.txt', 'r')
+
+    inputStacks = [f.readline(), f.readline(), f.readline(), f.readline(), f.readline(), f.readline(), f.readline(), f.readline()]
+    inputStacks.reverse()
+    f.readline() # numbers
+    f.readline() # blank line
+
+    stacks = [[], [], [], [], [], [], [], [], []]
+
+    for row in inputStacks:
+        tempRow = row
+        for i in range(0, 9):
+            block = tempRow[0:3]
+            if block[1] != " ":
+                stacks[i].append(block[1])
+            tempRow = tempRow[4:len(tempRow)]
+    
+    for instruction in f.readlines():
+        split = instruction.split(" ")
+        qty = int(split[1])
+        start = int(split[3]) - 1
+        end = int(split[5]) - 1
+        tempStack = []
+        for i in range(qty):
+            tempStack.append(stacks[start].pop())
+        tempStack.reverse()
+        for temp in tempStack:    
+            stacks[end].append(temp)
+
+    f.close()
+    # remaining code here
+    for stack in stacks:
+        print(stack.pop(), end="")
+    print()
+
+
+starttime = time.time()
+main()
+endtime = time.time()
+
+print("Runtime: {} seconds".format(round(endtime - starttime, 3)))
