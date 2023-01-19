d210b8d88debf8e0a3a76d69f93eb2bcad6b94c3
Dalton <daltonstwilliams@gmail.com>
2023-01-18 20:25:05 -0600

    2022 day 6

---
+++ 2022/6/1.py
@@ -0,0 +1,39 @@
+import time
+
+def main():
+    f = open('input.txt', 'r')
+
+    line = f.readline()
+    buffer = []
+    for i in range(len(line)):
+        curChar = line[i]
+        if i in [0, 1, 2]:
+            buffer.append(curChar)
+        else:
+            buffer.append(curChar)
+            if checkBuffer(buffer):
+                print(i + 1)
+                return
+            else:
+                buffer = buffer[1:]
+
+    f.close()
+
+    # remaining code here
+    
+
+def checkBuffer(buffer):
+    for i in range(0, 4):
+        char = buffer[i]
+        for j in range(i + 1, 4):
+            if buffer[j] == char:
+                print("{} - False".format(buffer))
+                return False
+    print("{} - True".format(buffer))
+    return True
+
+starttime = time.time()
+main()
+endtime = time.time()
+
+print("Runtime: {} seconds".format(round(endtime - starttime, 3)))
