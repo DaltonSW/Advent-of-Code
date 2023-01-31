01539777b72e2a4657ca156865dcb26f6a204e21
Dalton <daltonstwilliams@gmail.com>
2023-01-30 21:35:51 -0600

    2016 day 6

---
+++ 2016/6/2.py
@@ -0,0 +1,40 @@
+import time
+import aocd
+import os
+
+
+def main():
+    cwd = os.getcwd().split('\\')
+    data = aocd.get_data(None, int(cwd[-1]), int(cwd[-2]))
+    data = data.split('\n')
+
+    # f = open('test.txt', 'r')
+    # data = []
+    # for line in f.readlines():
+    #     data.append(line.strip())
+    # f.close()
+
+    dicts = [{}, {}, {}, {}, {}, {}, {}, {}, {}]
+    for row in data:
+        for i in range(1, 9):
+            addOrIncChar(dicts[i], row[i - 1])
+
+    for d in dicts:
+        if d == {}:
+            continue
+        sortedChars = list({k: v for k, v in sorted(d.items(), key=lambda item: item[1])})
+        print(sortedChars[0])
+    print(data)
+
+
+def addOrIncChar(dictionary: dict, char: str):
+    try:
+        dictionary[char] += 1
+    except KeyError:
+        dictionary[char] = 1
+
+starttime = time.time()
+main()
+endtime = time.time()
+
+print("Runtime: {} seconds".format(round(endtime - starttime, 3)))
