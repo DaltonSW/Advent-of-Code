7516aed9bdccf6f655f844e42ade0019da7f18da
Dalton <daltonstwilliams@gmail.com>
2023-12-07 18:16:08 -0600

    2023 day 7 part 1

---
+++ 2023/7/2.py
@@ -0,0 +1,17 @@
+import time
+import aocd
+import os
+import re
+
+
+def main():
+    cwd = os.getcwd().split('\\')
+    data = aocd.get_data(None, int(cwd[-1]), int(cwd[-2]))
+    print(data)
+
+
+starttime = time.time()
+main()
+endtime = time.time()
+
+print("Runtime: {} seconds".format(round(endtime - starttime, 3)))
